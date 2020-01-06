255ced433597f65bbfbdd48f13e30079f5c9780f
bondka <https://github.com/Siwarr>
2020-01-06 16:35:21 +0100

    first commit crud and db

--- src/main/java/com/example/demo/Location/LocationRepository.java
+++
@@ -1,8 +0,0 @@
-/*package com.example.demo.Location;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LocationRepository extends JpaRepository<Location, Integer> {
-
-}
-*/
